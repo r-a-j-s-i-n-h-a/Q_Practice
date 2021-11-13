6aecdb8512eed03c49a0502696af5f51c0333c43
r-a-j-s-i-n-h-a <94060322+r-a-j-s-i-n-h-a@users.noreply.github.com>
2021-11-13 12:17:25 +0530

    If-else java

---
+++ ifelse.java
@@ -0,0 +1,18 @@
+ public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        if(N%2==1)
+            System.out.println("Weird");
+        else
+            {
+            if(N>=2&&N<=5)
+                System.out.println("Not Weird");
+            else if(N<=20)
+                System.out.println("Weird");
+                else
+                System.out.println("Not Weird");
+        }
+    }
+}
