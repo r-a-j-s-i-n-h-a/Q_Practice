aaf20191752a777968962220517f6769b1c04bb7
r-a-j-s-i-n-h-a <94060322+r-a-j-s-i-n-h-a@users.noreply.github.com>
2021-11-27 23:29:44 +0530

    Practice Question

---
+++ Pattern.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+import java.util.Scanner;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner in = new Scanner(System.in);
+        int testCases = Integer.parseInt(in.nextLine());
+        while(testCases > 0){
+            String pattern = in.nextLine();
+            try {
+                Pattern.compile(pattern);
+                System.out.println("Valid");
+            } catch (PatternSyntaxException e) {
+                System.out.println("Invalid");
+            }
+            testCases--;
+        }
+    }
+}
