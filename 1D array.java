9dd6ee5118935ee7f90cc1cf9d0695d382668106
r-a-j-s-i-n-h-a <94060322+r-a-j-s-i-n-h-a@users.noreply.github.com>
2021-11-27 23:32:14 +0530

    array basic

---
+++ 1D array.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+
+
+public class Solution {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++)
+        arr[i]=sc.nextInt();
+        for(int i=0;i<n;i++)
+        System.out.println(arr[i]);
+    }
+}
